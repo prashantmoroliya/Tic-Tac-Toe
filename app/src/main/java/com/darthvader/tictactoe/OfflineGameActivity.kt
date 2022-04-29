44378cc1aa73592deead47ec510a67e437344f43
vader <prashantmoroliya@users.noreply.github.com>
2022-04-29 22:02:16 +0530

    activity names changed

--- app/src/main/java/com/darthvader/tictactoe/OfflineGameActivity.kt
+++ app/src/main/java/com/darthvader/tictactoe/OfflineGameActivity.kt
@@ -3,9 +3,9 @@ package com.darthvader.tictactoe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class OfflineGameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_offline_game)
     }
 }
